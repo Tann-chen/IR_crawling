4cb16137943dc0cf64513817f036280f3f856432
Tann <tianen.chen@outlook.com>
2017-11-23 20:49:18 -0500

    href

--- crawler.py
+++ crawler.py
@@ -2,37 +2,39 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def get_links_within_page(url):
-    global links_lst
-
+def get_links_within_page(url, links_container):
     current_page_links = []
 
     try:
-        response = requests.get(url, timeout=0.5)
+        response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         # print(soup.prettify())
         a_tags = soup.findAll('a')
 
         for a in a_tags:
-            link = a['href']
-            if link[:4] == 'http' or link[:3] == 'www':
-                current_page_links.append(link)
-                links_lst.append(link)
-            else:
-                current_page_links.append(link)
-                links_lst.append(url + link)  # relative path
+            if 'href' in a.attrs:
+                link = a['href']
+                if 'mailto' not in link:
+                    if link[:4] == 'http' or link[:3] == 'www':
+                        current_page_links.append(link)
+                        links_container.append(link)
+                    else:
+                        current_page_links.append(link)
+                        links_container.append(url + link)  # relative path
 
     except requests.exceptions.RequestException as e:
         print('*** one exception in request for crawling page ***')
         print(str(e))
 
-    return current_page_links
+    return
 
 
-def crawling(start_url, depth):
-
+# def crawling(start_url, count_limit):
 
 
 if __name__ == '__main__':
     links_lst = []
-    get_links_within_page('http://www.baidu.com')
+    get_links_within_page('https://csu.qc.ca/content/student-groups-associations', links_lst)
+
+    for link in links_lst:
+        print(link)
